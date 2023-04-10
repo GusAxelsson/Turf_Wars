3d442806f86c1c5dfdc65acfcfc2dc30d2333ec9
Markus Wagner <prttmprpht@gmail.com>
2023-04-10 19:22:05 +0200

    Add new planting/mowing behavior, change movement to work without acceleration

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,147 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject grassPrefab;
+    public float xStart = -9.5F;
+    public float yStart = 2.5F;
+    public float tileSize = 0.5F;
+    public int gridWidth = 40;
+    public int gridHeight = 16;
+    public float positioningOffset = 0.1F;
+    private GameObject[,] grid;
+    private int totalGrassTiles;
+
+    void Start()
+    {
+        grid = new GameObject[gridWidth, gridHeight];
+        totalGrassTiles = 0;
+        InitializeGrid();
+    }
+
+    void Update()
+    {
+        Debug.Log(GetGrassPercentage());   
+    }
+
+    /// <summary>
+    /// Instantiates a grass tile with slight randomness in position.
+    /// </summary>
+    private GameObject instantiateGrass(int x, int y)
+    {
+        float xOffset = Random.Range(-positioningOffset, positioningOffset);
+        float yOffset = Random.Range(-positioningOffset, positioningOffset);
+        return Instantiate(grassPrefab, new Vector3(xStart + x * tileSize + xOffset, yStart - y * tileSize - yOffset, 0), Quaternion.identity);
+    }
+
+    /// <summary>
+    /// Initializes the grid with grass tiles.
+    /// </summary>
+    private void InitializeGrid()
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            // Currently, what we're doing is that the left side of the screen has a higher probability to start out with grass.
+            float floatingProbability = ((float) (gridWidth - x) / gridWidth);
+            for (int y = 0; y < gridHeight; y++)
+            {
+                if (Random.Range(0F, 1F) < floatingProbability)
+                {
+                    GameObject grass = instantiateGrass(x, y);
+                    grid[x, y] = grass;
+                    totalGrassTiles++;
+                }
+                else
+                {
+                    grid[x, y] = null;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Converts world position to grid position.
+    /// </summary>
+    private Vector2Int WorldToGrid(float worldX, float worldY)
+    {
+        int x = Mathf.FloorToInt((worldX - xStart) / tileSize);
+        int y = Mathf.FloorToInt((yStart - worldY) / tileSize);
+        x = Mathf.Clamp(x, 0, gridWidth - 1);
+        y = Mathf.Clamp(y, 0, gridHeight - 1);
+        return new Vector2Int(x, y);
+    }
+
+    /// <summary>
+    /// Modifies an area in the grid around a certain position by either planting or mowing grass.
+    /// Depending on the player type, this will either create or destroy grass.
+    /// </summary>
+    private void AffectArea(Vector2 playerPosition, bool plant)
+    {
+        // Get the player's position within the grid.
+        Vector2Int gridPosition = WorldToGrid(playerPosition.x, playerPosition.y);
+        
+        // Helper variables for calculating the range of candidate tiles...
+        float radius = 0.7F;
+        int maxDistance = Mathf.CeilToInt(radius / tileSize);
+
+        // Calculate the range of candidate tiles.
+        int minX = Mathf.Clamp(gridPosition.x - maxDistance, 0, gridWidth - 1);
+        int maxX = Mathf.Clamp(gridPosition.x + maxDistance, 0, gridWidth - 1);
+        int minY = Mathf.Clamp(gridPosition.y - maxDistance, 0, gridHeight - 1);
+        int maxY = Mathf.Clamp(gridPosition.y + maxDistance, 0, gridHeight - 1);
+
+        // Go through all the candidate tiles and apply the desired behavior.
+        for (int x = minX; x <= maxX; x++)
+        {
+            for (int y = minY; y <= maxY; y++)
+            {
+                // GUARD: We do not change anything if the tile is already in its desired state.
+                if ((!plant && grid[x, y] == null) || (plant && grid[x, y] != null))
+                {
+                    continue;
+                }
+
+                // Now we need to calculate the actual distance of the candidate tile to the player,
+                // to check if it is really within their radius.
+                Vector2 grassPosition = new Vector2(xStart + x * tileSize, yStart - y * tileSize);
+                float distance = Vector2.Distance(playerPosition, grassPosition);
+
+                // GUARD: Bail, if the player is too far away.
+                if (distance > radius)
+                {
+                    continue;
+                }
+
+                if (plant)
+                {
+                    // Create a new grass tile if the player is a planter.
+                    GameObject grass = instantiateGrass(x, y);
+                    grid[x, y] = grass;
+                    totalGrassTiles++;
+                } else
+                {
+                    // Remove a pre-existing grass tile if the player is a mower.
+                    Destroy(grid[x, y]);
+                    grid[x, y] = null;
+                    totalGrassTiles--;
+                }
+            }
+        }
+    }
+
+    public void Mow(Vector2 playerPosition)
+    {
+        AffectArea(playerPosition, false);
+    }
+
+    public void Plant(Vector2 playerPosition)
+    {
+        AffectArea(playerPosition, true);
+    }
+
+    public float GetGrassPercentage()
+    {
+        return (float)totalGrassTiles / (gridWidth * gridHeight) * 100F;
+    }
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,14 +7,14 @@ using UnityEngine.InputSystem;
 public class playerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float acceleration = 1f;
-    public float maxSpeed = 100f;
-    Vector2 momentum;
+    public float maxSpeed = 5f;
     Vector2 movementDirection;
-    Vector2 move;
+    Vector2 lastAnimDirection = new Vector2(0, 1);
+    public bool logMovement = false;
     public InputAction playerControls;
     public Animator animator;
-
+    public TileManager tileManager;
+    public bool plant;
 
     // This section is recommended by documentation
     private void OnEnable()
@@ -26,23 +26,54 @@ public class playerMovement : MonoBehaviour
         playerControls.Disable();
     }
     ////////////////
-    
 
+    /// <summary>
+    /// Ensures that a vector does not include diagonal movement.
+    /// </summary>
+    private Vector2 simplifyDirectionalVector(Vector2 direction)
+    {
+        Vector2 newDirection = new Vector2(direction.x, direction.y);
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            newDirection.x = Mathf.Sign(direction.x) * direction.magnitude;
+            newDirection.y = 0;
+        }
+        else
+        {
+            newDirection.x = 0;
+            newDirection.y = Mathf.Sign(direction.y) * direction.magnitude;
+        }
+        return newDirection;
+    }
 
     // Update is called once per frame
     void Update()
     {
         // read direction from playerControls
         movementDirection = playerControls.ReadValue<Vector2>();
+        movementDirection = simplifyDirectionalVector(movementDirection);
+
+        // Only change sprite on actual movement.
+        if (movementDirection.magnitude > 0)
+        {
+            lastAnimDirection = movementDirection;
+        }
+
+        if (logMovement) Debug.Log(lastAnimDirection);
         // Send directional info to the animator
-        animator.SetFloat("Horizontal", movementDirection.x);
-        animator.SetFloat("Vertical", movementDirection.y);
-        animator.SetFloat("Speed", movementDirection.sqrMagnitude);
+        animator.SetFloat("Horizontal", lastAnimDirection.x);
+        animator.SetFloat("Vertical", lastAnimDirection.y);
+        animator.SetFloat("Speed", lastAnimDirection.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        momentum += movementDirection * acceleration;
+
+        Vector2 momentum = movementDirection * maxSpeed;
+        if (logMovement)
+        {
+            Debug.Log(momentum);
+        }
         // If we are almost standing still. kill momentum
         if (movementDirection.magnitude < 0.5)
         {
@@ -55,19 +86,20 @@ public class playerMovement : MonoBehaviour
             momentum = momentum.normalized * maxSpeed;
         }
         // make sure that we are always only moving in the (nwse) directions
-        if (Mathf.Abs(movementDirection.x) > Mathf.Abs(movementDirection.y)) {
-            move.x = momentum.x;
-            move.y = 0;
-            momentum.y = 0;
-        }
-        else
+
+        // If we are going to move, we're properly triggering the mowing/planting depending on player type.
+        if (momentum.magnitude > 0)
         {
-            move.x = 0;
-            move.y = momentum.y;
-            momentum.x = 0;
+            if (plant)
+            {
+                tileManager.Plant(rb.position);
+            } else
+            {
+                tileManager.Mow(rb.position);
+            }
         }
+
         // Apply movement
-        rb.MovePosition(rb.position + move * Time.fixedDeltaTime);
-        Debug.Log(move);
+        rb.MovePosition(rb.position + momentum * Time.fixedDeltaTime);
     }
 }
