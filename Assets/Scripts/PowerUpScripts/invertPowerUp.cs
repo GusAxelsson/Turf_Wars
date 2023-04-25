e058191fbacc8d0c8e56f1ac81ea62548abd90af
Gustav Axelsson <gustavax@kth.se>
2023-04-25 12:20:30 +0200

    Fixed bug with powers
    powers sometimes triggering twice on enter

--- Assets/Scripts/PowerUpScripts/invertPowerUp.cs
+++ Assets/Scripts/PowerUpScripts/invertPowerUp.cs
@@ -6,19 +6,22 @@ public class invertPowerUp : MonoBehaviour
 {
    
     public GameObject powerUpScript;
-
+    private bool triggered = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player1")
-        {
-            GameObject.FindGameObjectWithTag("Player2").GetComponent<playerMovement>().powerUpCollected(2);
+        if (triggered == false){
+            triggered = true;
+            if (collision.tag == "Player1")
+            {
+                GameObject.FindGameObjectWithTag("Player2").GetComponent<playerMovement>().powerUpCollected(2);
+            }
+            if (collision.tag == "Player2")
+            {
+                GameObject.FindGameObjectWithTag("Player1").GetComponent<playerMovement>().powerUpCollected(2);
+            }
+            GameObject.Find("PowerupSpawner").GetComponent<PowerupSpawned>().totalPowerups -= 1;
+            Destroy(gameObject);
         }
-        if (collision.tag == "Player2")
-        {
-            GameObject.FindGameObjectWithTag("Player1").GetComponent<playerMovement>().powerUpCollected(2);
-        }
-        GameObject.Find("PowerupSpawner").GetComponent<PowerupSpawned>().totalPowerups -= 1;
-        Destroy(gameObject);
     }
 
 }
