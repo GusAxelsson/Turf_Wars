cf2e08fd40ff61a41ab8c63679617ea445a76947
Markus Wagner <prttmprpht@gmail.com>
2023-05-17 19:07:31 +0200

    Add volcanic eruption

---
+++ Assets/Scripts/ImpendingDoomController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpendingDoomController : MonoBehaviour
+{
+    public GameObject player1; //lawn
+    public GameObject player2; //plant
+
+    public GameObject meteor;
+    public GameObject shadow; 
+    public float speed = 1.0F;
+
+    private float totalDistance;
+
+    void Start()
+    {
+        totalDistance = (meteor.transform.position - transform.position).magnitude;
+        player1 = GameObject.FindGameObjectWithTag("Player1");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
+    }
+
+    void Update()
+    {
+        float distance = (meteor.transform.position - transform.position).magnitude;
+        float opacity = 1.0F - distance / totalDistance;
+
+        shadow.GetComponent<SpriteRenderer>().color = new Vector4(1.0F, 1.0F, 1.0F, opacity);
+
+        float step = speed * Time.deltaTime;
+        meteor.transform.position = Vector3.MoveTowards(meteor.transform.position, transform.position, step);
+
+        if (meteor.transform.position == transform.position)
+        {
+            hitPlayer();
+            Destroy(this.gameObject);
+        }
+    }
+
+    void hitPlayer()
+    {
+        if ((player1.transform.position - transform.position).magnitude < 1.5F)
+        {
+            player1.GetComponent<MovementController>().powerUpCollected(3);
+        }
+
+
+        if ((player2.transform.position - transform.position).magnitude < 1.5F)
+        {
+            player2.GetComponent<MovementController>().powerUpCollected(3);
+        }
+    }
+}
---
+++ Assets/Scripts/VolcanoEruptionController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoEruptionController : MonoBehaviour
+{
+    public GameObject impendingDoomPrefab;
+    public GameObject targetObject;
+    public float spawnRadius = 2.0f;
+    public float spawnRate = 0.3f;
+    public float timer = 20.0f;
+    private float spawnTimer = 0.0f;
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        spawnTimer -= Time.deltaTime;
+
+        if (timer > 0)
+        {
+            if (spawnTimer <= 0)
+            {
+                spawnTimer = spawnRate;
+                Vector2 spawnPoint = Random.insideUnitCircle * spawnRadius;
+                Vector3 spawnPosition = new Vector3(
+                    targetObject.transform.position.x + spawnPoint.x,
+                    targetObject.transform.position.y + spawnPoint.y,
+                    targetObject.transform.position.z
+                );
+                Instantiate(impendingDoomPrefab, spawnPosition, Quaternion.identity);
+            }
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
